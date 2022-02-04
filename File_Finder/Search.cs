7ab93187a91dff8f722938ee80114fb0311c94db
WUPQTA - (Chase A) <chasealexander@ussupi.com>
2022-02-04 07:21:11 -0800

    Refactored search functions

--- File_Finder/Search.cs
+++ File_Finder/Search.cs
@@ -10,13 +10,14 @@ namespace File_Finder {
         private string[] fileTypes;
         private string path;
 
-        //Constructor
+        //***** Constructor *****//
         public Search(string path, string fileTypes) {
             this.path = path;
             this.fileTypes = fileTypes.Split(',');
         }
 
-        //Non-recursive phrase search
+
+        //***** Non-recursive phrase search *****//
         public List<string> phraseSearch(string searchTerm) {
             List<string> foundFiles = new List<string>();
 
@@ -38,7 +39,8 @@ namespace File_Finder {
             return foundFiles;      
         }
 
-        //Recursive phrase search
+
+        //***** Recursive phrase search *****//
         public List<string> phraseSearchRecur(string searchTerm, string path) {
             List<string> foundFiles = new List<string>();
 
@@ -47,50 +49,26 @@ namespace File_Finder {
                 foundFiles.AddRange(phraseSearchRecur(searchTerm, directory));
             }
 
-            //For each file type
-            foreach (var type in fileTypes) {
-
-                //Get all file names of the current type that contain the search term
-                var fileList = Directory.GetFiles(path, "*" + type);
-
-                //Get all filenames that contain the search term
-                foreach (string filepath in fileList) {
-                    string filename = filepath.Split("\\").Last();
-                    if (filename.Contains(searchTerm)) {
-                        foundFiles.Add(filepath);  //Append the found file names to temp found
-                    }
-                }
-            }
+            foundFiles.AddRange(phraseSearch(searchTerm));  //Find matches
 
             return foundFiles;
         }
 
-        //Non-recursive range search
+
+        //***** Non-recursive range search *****//
         public List<string> rangeSearch(int lower, int upper) {
             List<string> foundFiles = new List<string>();
 
             //For each number in the range
             for (int searchTerm = lower; searchTerm <= upper; searchTerm++) {
-
-                //For each file type
-                foreach (var type in fileTypes) {
-
-                    //Get all files of the current type
-                    var fileList = Directory.GetFiles(path, "*" + type);
-
-                    //Get all filenames that contain the search term
-                    foreach (string filepath in fileList) {
-                        string filename = filepath.Split("\\").Last();
-                        if ( filename.Contains(searchTerm.ToString()) ) {
-                            foundFiles.Add(filepath);  //Append the found file names to temp found
-                        }
-                    }
-                }
+                foundFiles.AddRange( phraseSearch(searchTerm.ToString()) );  //Find matches
             }
+
             return foundFiles;
         }
 
-        //Recursive range search
+
+        //***** Recursive range search *****//
         public List<string> rangeSearchRecur(int lower, int upper, string path) {
             List<string> foundFiles = new List<string>();
 
@@ -102,25 +80,12 @@ namespace File_Finder {
                     foundFiles.AddRange(rangeSearchRecur(lower, upper, directory));
                 }
 
-                //For each file type
-                foreach (var type in fileTypes) {
-
-                    //Get all files of the current type
-                    var fileList = Directory.GetFiles(path, "*" + type);
-
-                    //Get all filenames that contain the search term
-                    foreach (string filepath in fileList) {
-                        string filename = filepath.Split("\\").Last();
-                        if ( filename.Contains(searchTerm.ToString()) ) {
-                            foundFiles.Add(filepath);  //Append the found filenames to temp found
-                        }
-                    }
-                }
+                foundFiles.AddRange( phraseSearch(searchTerm.ToString()) );  //Find matches
             }
 
             return foundFiles;
         }
 
-        //TODO: Refactor by inserting non-recursive range search into other functions
+
     }
 }
