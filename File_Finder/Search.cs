8b52a80d9f6ece1d1fc8d42bb5e3ff1b4c110963
WUPQTA - (Chase A) <chasealexander@ussupi.com>
2022-02-11 10:33:26 -0800

    Fixed duplicate issue for nonrecursive range search

--- File_Finder/Search.cs
+++ File_Finder/Search.cs
@@ -110,8 +110,10 @@ namespace File_Finder {
                     foreach (string filepath in fileList) {
                         string filename = filepath.Split("\\").Last();
                         if (filename.Contains(searchTerm.ToString())) {
-                            results.Add(filepath, true);  //Append the found file names to temp found
-                            System.Diagnostics.Debug.WriteLine("Added " + filepath);
+                            if (!results.ContainsKey(filepath)) {
+                                results.Add(filepath, true);  //Append the found file
+                                System.Diagnostics.Debug.WriteLine("Added " + filepath);
+                            }
                         }
                     }
                 }
