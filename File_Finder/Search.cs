c7cbfcbb03904af43eb5fa911a7b259002e65a39
WUPQTA - (Chase A) <chasealexander@ussupi.com>
2022-02-11 13:08:41 -0800

    Greatly improved search time for recursive range search

--- File_Finder/Search.cs
+++ File_Finder/Search.cs
@@ -141,42 +141,45 @@ namespace File_Finder {
         public Dictionary<string, bool> rangeSearchRecur(int lower, int upper, string path) {
             Dictionary<string, bool> results = new Dictionary<string, bool>();
 
-            //For each number in the range
-            for (int searchTerm = lower; searchTerm <= upper; searchTerm++) {
+            //For each found directory do a recursive range search
+            foreach (var directory in Directory.GetDirectories(path)) {
+                Dictionary<string, bool> subdirResults = rangeSearchRecur(lower, upper, directory);
 
-                //For each found directory do a recursive range search
-                foreach (var directory in Directory.GetDirectories(path)) {
-                    Dictionary<string, bool> subdirResults = rangeSearchRecur(lower, upper, directory);
-
-                    //Remove any non-detections that may have been added by a subdirectory
-                    foreach (var item in subdirResults.Where(x => x.Value == false).ToList()) {
-                        subdirResults.Remove(item.Key);
-                    }
-
-                    //Combine the subdirectory results with the overall results
-                    subdirResults.ToList().ForEach(x => results[x.Key] = x.Value);
+                //Remove any non-detections that may have been added by a subdirectory
+                foreach (var item in subdirResults.Where(x => x.Value == false).ToList()) {
+                    subdirResults.Remove(item.Key);
                 }
 
-                //For each file type
-                foreach (var type in fileTypes) {
+                //Combine the subdirectory results with the overall results
+                subdirResults.ToList().ForEach(x => results[x.Key] = x.Value);
+            }
 
-                    //Get all file names of the current type that contain the search term
-                    var fileList = Directory.GetFiles(path, "*" + type);
+            //For each file type
+            foreach (var type in fileTypes) {
 
-                    //Get all filenames that contain the search term
-                    foreach (string filepath in fileList) {
-                        string filename = filepath.Split("\\").Last();
-                        if ( filename.Contains(searchTerm.ToString()) ) {
+                //Get all file names of the current type that contain the search term
+                var fileList = Directory.GetFiles(path, "*" + type);
+
+                //Get all filenames that contain the search term
+                foreach (string filepath in fileList) {
+                    string filename = filepath.Split("\\").Last();
+
+                    //For each number in the range
+                    for (int searchTerm = lower; searchTerm <= upper; searchTerm++) {
+                        if (filename.Contains(searchTerm.ToString())) {
                             if (!results.ContainsKey(filepath)) {
                                 results.Add(filepath, true);  //Append the found file
                                 System.Diagnostics.Debug.WriteLine("Added " + filepath);
-                            } 
+                            }
                         }
                     }
                 }
+            }
 
+            //If term was not found
+            for (int searchTerm = lower; searchTerm <= upper; searchTerm++) {
                 bool notFound = true;
-                foreach(var entry in results) {
+                foreach (var entry in results) {
                     if (entry.Key.Contains(searchTerm.ToString())) {
                         notFound = false;
                         break;
@@ -186,13 +189,11 @@ namespace File_Finder {
                 if (notFound) {
                     results.Add(searchTerm.ToString(), false);
                 }
-                
-
             }
+
 
             return results;
         }
-
 
     }
 }
